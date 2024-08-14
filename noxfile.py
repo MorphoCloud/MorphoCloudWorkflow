2bab68b1daf42dc361b74c6e4b0e4c5e12376299
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2024-08-14 17:02:28 -0400

    feat: Add "vendorize" noxfile session

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,7 @@
+import argparse
+import shutil
+from pathlib import Path
+
 import nox
 
 nox.needs_version = ">=2024.3.2"
@@ -12,3 +16,44 @@ def lint(session: nox.Session) -> str:
     """
     session.install("pre-commit")
     session.run("pre-commit", "run", "-a")
+
+
+def _vendorize(session: nox.Session, paths: list[str]) -> None:
+    """
+    Vendorize files into a directory. Directory must exist.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "target", type=Path, help="The target directory to vendorize file into."
+    )
+    args = parser.parse_args(session.posargs)
+
+    if not args.target.is_dir():
+        msg = f"Target directory {args.target} does not exist"
+        raise AssertionError(msg)
+
+    src_dir = Path(__file__).parent
+    target_dir = args.target
+
+    for path in paths:
+        src_path = src_dir / path
+        target_path = target_dir / path
+        if src_path.is_dir():
+            session.log(f"Copying directory {src_path} -> {target_path}")
+            shutil.copytree(src_path, target_path, dirs_exist_ok=True)
+        else:
+            session.log(f"Copying file {src_path} -> {target_path}")
+            shutil.copy2(src_path, target_path)
+
+
+@nox.session
+def vendorize(session: nox.Session) -> None:
+    _vendorize(
+        session,
+        [
+            ".github",
+            ".pre-commit-config.yaml",
+            "issue-commands.md",
+            "cloud-config",
+        ],
+    )
